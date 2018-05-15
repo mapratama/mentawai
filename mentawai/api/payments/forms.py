567ad0c50ce39eb36bab074daa15240ce4b48625
mapratama <muh.angga06@gmail.com>
2018-05-15 22:09:56 +0700

    update form

--- mentawai/api/payments/forms.py
+++ mentawai/api/payments/forms.py
@@ -6,13 +6,14 @@ from mentawai.apps.payment_histories.models import PaymentHistory
 class PaymentForm(forms.Form):
     amount = forms.FloatField()
     number_of_visits = forms.IntegerField()
+    payment_id = forms.CharField()
 
     def save(self, user):
         payment = user.payment_histories.create(
             value=self.cleaned_data['amount'],
-            payment_id='12345678',
+            payment_id=self.cleaned_data['payment_id'],
             number_of_visits=self.cleaned_data['number_of_visits'],
-            status=PaymentHistory.STATUS.completed
+            status=PaymentHistory.STATUS.new
         )
 
         return payment
