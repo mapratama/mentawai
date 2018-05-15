ad81361b4a2417860892aaf2c6d3e0af37d5a42e
Rudi Muliawan <rudi@ui.co.id>
2018-05-15 22:33:29 +0700

    fixes charge_doku functionality

--- mentawai/api/payments/views.py
+++ mentawai/api/payments/views.py
@@ -6,6 +6,8 @@ from mentawai.core.utils import charge_doku
 from rest_framework import status
 from rest_framework.response import Response
 
+from mentawai.core.exceptions import DokuPaymentError
+
 from .forms import PaymentForm
 
 
@@ -15,6 +17,11 @@ class Pay(SessionAPIView):
         form = PaymentForm(data=request.data)
         if form.is_valid():
             payment = form.save(request.user)
-            charge_doku(request.data, payment)
+
+            try:
+                charge_doku(request.data, payment)
+            except DokuPaymentError as error:
+                return ErrorResponse(error_description=error)
+
             return Response(serialize_payment(payment), status=status.HTTP_200_OK)
         return ErrorResponse(form=form)
--- mentawai/core/utils.py
+++ mentawai/core/utils.py
@@ -1,4 +1,5 @@
 import hashlib
+import json
 import os
 import requests
 
@@ -7,7 +8,11 @@ from django.contrib.auth import login
 from django.template.defaultfilters import slugify
 from django.utils import timezone
 
+from requests.exceptions import ConnectionError, Timeout
+
 import phonenumbers
+
+from mentawai.core.exceptions import DokuPaymentError
 
 
 class FilenameGenerator(object):
@@ -60,9 +65,9 @@ def create_word(amount, invoice, token, pairing_code, device_id):
         invoice, '360', token, pairing_code, device_id)).hexdigest()
 
 
-def charge_doku(data, invoice):
+def charge_doku(data):
     words = create_word(
-        data['res_amount'], invoice, data['res_token_id'],
+        data['res_amount'], data['res_transaction_id'], data['res_token_id'],
         data['res_pairing_code'], data['res_device_id']
     )
 
@@ -72,10 +77,9 @@ def charge_doku(data, invoice):
         'req_mall_id': settings.DOKU_MERCHANT_ID,
         'req_chain_merchant': 'NA',
         'req_amount': data['res_amount'],
-        'req_trans_id_merchant': data['res_amount'],
         'req_words': words,
         'req_purchase_amount': data['res_amount'],
-        'req_trans_id_merchant': invoice,
+        'req_trans_id_merchant': data['res_transaction_id'],
         'req_request_date_time': current_time.strftime('%Y%m%d%H%I%S'),
         'req_currency': '360',
         'req_purchase_currency': '360',
@@ -85,10 +89,23 @@ def charge_doku(data, invoice):
         'req_basket': '%s,%s,%s,%s' % ('retribusi', data['res_amount'], '1', data['res_amount']),
         'req_address': 'Bogor',
         'req_email': data['res_data_email'],
-        'req_token_id': data['res_token_id']
+        'req_token_id': data['res_token_id'],
+        'req_mobile_phone': '0858194234321',
     }
 
-    params = str(params)
-    response = requests.post(url=settings.DOKU_PAYMENT_URL, data={'data': params})
+    params = json.dumps(params).strip(' ')
+
+    try:
+        response = requests.post(url=settings.DOKU_PAYMENT_URL,
+                                 data={'data': params})
+        response_json = response.json()
+    except (ConnectionError, Timeout):
+        raise DokuPaymentError("Connection Timeout, can't connect to DOKU")
+
+    if response.status_code != 200:
+        raise DokuPaymentError('Invalid Data')
+
+    if response_json['res_response_msg'] != 'SUCCESS':
+        raise DokuPaymentError("Error from DOKU, unable to charge")
 
     return response
